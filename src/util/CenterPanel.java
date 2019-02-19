adb8a67cdecd14c807ee62f78989ed4d98dec242
srdczk <617471633@qq.com>
2019-02-19 16:45:44 +0800

    为何idea上传文件不全

--- src/util/CenterPanel.java
+++ src/util/CenterPanel.java
@@ -81,5 +81,6 @@ public class CenterPanel extends JPanel{
         }));
         centerPanel.show(jButton);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //每次要重新初始化git仓库?
     }
 }
